183da9f562b79d960208378aa02fe89c0cdaea83
daven.hughes <daven.hughes@92c3b6dc-493d-11de-82d9-516ade3e46db>
2009-06-09 01:52:19 +0000

    TRY THAT

--- main.cpp
+++ main.cpp
@@ -1,69 +1,63 @@
-/*
- * main.cpp
- *
- *  Created on: Jun 8, 2009
- *      Author: Corey
- */
 
 #define GLUT_DISABLE_ATEXIT_HACK
 #include <windows.h>
-#include <GL\glut.h>
+#include <GL/glut.h>
 
-void init(int,char**);
+int side = 100;
 
+void init(int, char**);
+void display();
+void keyboard(unsigned char key, int x, int y);
 void idle();
 
-void keyboard(unsigned char key, int x, int y);
+int main(int c, char**argv)
+{
+	init(c, argv);
 
-float testoperator = 5;
+	return 0;
+}
 
- int main(int c, char** argv)
- {
+void init(int c, char**argv)
+{
+	glutInit(&c, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+	glutCreateWindow(argv[0]);
+	glutFullScreen();
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutIdleFunc(idle);
+	glutMainLoop();
+}
 
-	 init(c, argv);
-	 return 0;
- }
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
 
- void display();
 
- void init(int n, char** argv)
- {
+	glColor3f(0.8f, 0.3f, 0.6f);
+	glBegin(GL_QUADS);
 
-	 glutInit(&n, argv);
-	 glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-	 glutCreateWindow(argv[0]);
-	 glutFullScreen();
-	 glutDisplayFunc(display);
-	 glutIdleFunc(idle);
-	 glutKeyboardFunc(keyboard);
-	 glutMainLoop();
- }
+	glVertex2f(0, 0);
+	glVertex2f(0, side);
+	glVertex2f(side, side);
+	glVertex2f(side, 0);
 
- void idle()
- {
-	 testoperator++;
-	 if (testoperator > 5)
-	 {
-		 testoperator = 0;
-	 }
-	 glutPostRedisplay();
- }
+	glEnd();
 
- void display()
- {
-	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	 glClearColor(0.5, 0.5, 0.5, 0.0);
-	 glColor3f(0.8f,0.3f,0.6f);
-	 glBegin(GL_QUADS);
-	 glVertex2f(testoperator,testoperator);
-	 glVertex2f(testoperator,testoperator*20);
-	 glVertex2f(testoperator*20,testoperator*20);
-	 glVertex2f(testoperator*20,testoperator);
-	 glEnd();
-	 glutSwapBuffers();
- }
+	glutSwapBuffers();
+}
 
- void keyboard(unsigned char key, int x, int y)
- {
-	 if(key == 27) exit(0);
- }
+void keyboard(unsigned char key, int x, int y)
+{
+	if(key == 27) exit(0);
+}
+
+void idle()
+{
+	side++;
+
+	if(side > 200) side = 100;
+
+	glutPostRedisplay();
+}
