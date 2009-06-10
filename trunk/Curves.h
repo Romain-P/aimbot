52c232c322c6bd9c3a2085e331482947df94428d
daven.hughes <daven.hughes@92c3b6dc-493d-11de-82d9-516ade3e46db>
2009-06-10 00:53:38 +0000

    Deleted extraneous stuff

--- trunk/Curves.h
+++ trunk/Curves.h
@@ -9,20 +9,6 @@
 #include <vector>
 #include <math.h>
 
-class crd
-{
-public:
-	//crd(float x, y) { this->x = x; this->y = y; }
-
-	float x, y;
-	void operator=(const crd& other)
-	{
-		this->x = other.x;
-		this->y = other.y;
-	}
-};
-
-
 void init(int c, char** argv);
 void initLight();
 void display();
@@ -32,16 +18,6 @@ void idle();
 void reshape(int x, int y);
 void keyboard(unsigned char key, int x, int y);
 void specialKey(int key, int x, int y);
-void drawAxes();
-void calcTable();
-void calcTable2();
-void calcCurves();
-void drawCurve();
-void drawSurface();
-void drawPoints();
-void drawLine();
-float yValue(int theta, int index);
-float dist(const crd& a, const crd& b);
-float dist2(const crd& a, const crd& b);
+
 
 #endif
